c026eb3d0638fb68fae47011ad82511d68a87967
seaseeseasee <nongsea1745217655@gmail.com>
2023-10-05 14:52:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <RouterView></RouterView>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import all_show from '../components/all_show.vue'
+</script>
+
+<template>
+  <main>
+    <all_show></all_show>
+  </main>
+</template>
