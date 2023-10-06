25c24592176234f7efd1eff19b4b923fd52d28bf
bossth192 <mju6504101307@mju.ac.th>
2023-10-06 14:17:56 +0700

    เพิ่มcoment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import all_show from '../components/all_show.vue'
 import Head_shop from '../components/Head_shop.vue'
 </script>
 
+<!--main item -->
 <template>
   <Head_shop></Head_shop><hr>
   <main>
